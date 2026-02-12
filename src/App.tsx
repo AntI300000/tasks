2743a0648d6a6e6d5082a103e3a4ea752300d48c
Anthony Imperiale <anthonyi@udel.edu>
2026-02-12 16:41:46 -0500

    Added Rows and Cols

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { Button } from "react-bootstrap";
+import { Button, Container, Row, Col } from "react-bootstrap";
 
 import img from "./assets/images/sailing_photo.jpg";
 
@@ -12,27 +12,45 @@ function App(): React.JSX.Element {
                 {/* <br />By Anthony Imperiale */}
                 <h2>By Anthony Imperiale</h2>
             </header>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
-            <p>Some of my hobbies</p>
-            <ul>
-                <li>Playing games</li>
-                <li>Model Painting</li>
-                <li>Sailing</li>
-            </ul>
-            <p>Here is a photo of me out on the water</p>
-            <img src={img} alt="Me at sailing practice" width="250" />
-            <div>
-                <Button
-                    onClick={() => {
-                        console.log("Hello World!");
-                    }}
-                >
-                    Log Hello World
-                </Button>
-            </div>
+            <Container>
+                <Row>
+                    <Col>
+                        <p>
+                            Edit <code>src/App.tsx</code> and save. This page
+                            will automatically reload.
+                        </p>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <p>Some of my hobbies</p>
+                        <ul>
+                            <li>Playing games</li>
+                            <li>Model Painting</li>
+                            <li>Sailing</li>
+                        </ul>
+                    </Col>
+                    <Col>
+                        <p>Here is a photo of me out on the water</p>
+                        <img
+                            src={img}
+                            alt="Me at sailing practice"
+                            width="250"
+                        />
+                    </Col>
+                </Row>
+                <Row>
+                    <div>
+                        <Button
+                            onClick={() => {
+                                console.log("Hello World!");
+                            }}
+                        >
+                            Log Hello World
+                        </Button>
+                    </div>
+                </Row>
+            </Container>
         </div>
     );
 }
