0d01e75ba9617a43b52617c1355d32e88c6f01c4
Anthony Imperiale <anthonyi@udel.edu>
2026-03-08 19:47:56 -0400

    StartAttempt half working

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,46 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): React.JSX.Element {
-    return <div>Start Attempt</div>;
+    const [numOfAttempts, setNumOfAttempts] = useState<number>(4);
+    const [inProgress, setInPogress] = useState<boolean>(false);
+
+    function changeInProgress(): void {
+        setInPogress(!inProgress);
+    }
+
+    function changeNumOfAttempts(changeBy: number): void {
+        setNumOfAttempts(numOfAttempts + changeBy);
+    }
+
+    return (
+        <span>
+            <Button
+                onClick={() => {
+                    changeInProgress();
+                }}
+                disabled={inProgress}
+            >
+                Start Quiz
+            </Button>
+
+            <Button
+                onClick={() => {
+                    changeInProgress();
+                }}
+                disabled={!inProgress}
+            >
+                Stop Quiz
+            </Button>
+
+            <Button
+                onClick={() => {
+                    changeNumOfAttempts(1);
+                }}
+                disabled={inProgress}
+            >
+                Mulligan
+            </Button>
+            <p>You have {numOfAttempts} left</p>
+        </span>
+    );
 }
