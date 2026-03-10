7b22fd047987dbe19e96c341a0651f538bcecdf6
Anthony Imperiale <anthonyi@udel.edu>
2026-03-09 20:38:56 -0400

    Finised Task 9

--- src/bad-components/DoubleHalf.tsx
+++ src/bad-components/DoubleHalf.tsx
@@ -1,33 +1,44 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
-//import { dhValue, setDhValue } from "./DoubleHalfState";
+
+function Doubler({
+    dhValue,
+    setDhValue,
+}: {
+    dhValue: number;
+    setDhValue: React.Dispatch<React.SetStateAction<number>>; //unsure what to put for the type of setDhValue, so I used what it showed in VS Code when I hovered over it.
+}): React.JSX.Element {
+    return (
+        <Button
+            onClick={() => {
+                setDhValue(2 * dhValue);
+            }}
+        >
+            Double
+        </Button>
+    );
+}
+
+function Halver({
+    dhValue,
+    setDhValue,
+}: {
+    dhValue: number;
+    setDhValue: React.Dispatch<React.SetStateAction<number>>;
+}): React.JSX.Element {
+    return (
+        <Button
+            onClick={() => {
+                setDhValue(0.5 * dhValue);
+            }}
+        >
+            Halve
+        </Button>
+    );
+}
 
 export function DoubleHalf(): React.JSX.Element {
     const [dhValue, setDhValue] = useState<number>(10);
-
-    function Doubler(): React.JSX.Element {
-        return (
-            <Button
-                onClick={() => {
-                    setDhValue(2 * dhValue);
-                }}
-            >
-                Double
-            </Button>
-        );
-    }
-
-    function Halver(): React.JSX.Element {
-        return (
-            <Button
-                onClick={() => {
-                    setDhValue(0.5 * dhValue);
-                }}
-            >
-                Halve
-            </Button>
-        );
-    }
 
     return (
         <div>
@@ -35,8 +46,8 @@ export function DoubleHalf(): React.JSX.Element {
             <div>
                 The current value is: <span>{dhValue}</span>
             </div>
-            <Doubler></Doubler>
-            <Halver></Halver>
+            <Doubler dhValue={dhValue} setDhValue={setDhValue} />
+            <Halver dhValue={dhValue} setDhValue={setDhValue} />
         </div>
     );
 }
